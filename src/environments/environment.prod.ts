f15024d13fa4d2d3ac0e1f43f603f1421fbfaae0
pedrito120 <pedrouriel_@hotmail.com>
2021-12-06 11:37:20 -0600

    MODIFY environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: {
+    api: 'https://pokeapi.co/api/v2/',
+    pokemon: {
+      id: 'pokemon/_id_',
+      nombre: 'pokemon/_name_'
+    },
+    type: {
+      id: 'type/_id_'
+    },
+    ability: {
+      id: 'ability/_id_'
+    }
+  }
 };
